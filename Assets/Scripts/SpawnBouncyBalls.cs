fc049116a52c1fde44be74cab6654f012313c453
Precious-Favour Jagun <jagunp@uchicago.edu>
2022-11-03 15:15:32 -0500

    Tutorial

--- Assets/Scripts/SpawnBouncyBalls.cs
+++ Assets/Scripts/SpawnBouncyBalls.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,13 +11,12 @@ public class SpawnBouncyBalls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i=0; i < numberOfBalls; i++)
-        {
+        for(int i=0; i<numberOfBalls; i++){
             Vector3 ballPosition = new Vector3(
                 Random.Range(-10f, 10f),
                 heightAboveGround,
-                Random.Range(-10f, 10f)
-                );
+                Random.Range(-10f,10f)
+            );
             Instantiate(bouncyBallsPrefab, ballPosition, Quaternion.identity);
         }
     }
